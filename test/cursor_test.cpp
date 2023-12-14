f040cf51a07a695a6f2642f52ea46127df71ba84
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2023-12-14 14:13:36 +0900

    test: :white_check_mark: config CMake to build test binaries

---
+++ test/cursor_test.cpp
@@ -0,0 +1,73 @@
+#include "hardware/spi.h"
+#include "pico/stdlib.h"
+#include <cstdio>
+
+#include "LCD_buffer.hpp"
+#include "color.hpp"
+#include "display.hpp"
+
+// SPI Defines
+// We are going to use SPI 0, and allocate it to the following GPIO pins
+// Pins can be changed, see the GPIO function select table in the datasheet for
+// information on GPIO assignments
+#define SPI_PORT spi0
+#define PIN_MISO 16
+#define PIN_CS 17
+#define PIN_SCK 18
+#define PIN_MOSI 19
+
+#define PIN_DC 2
+#define PIN_RST 3
+#define PIN_BL 4
+
+#define SCAN_DIR D2U_L2R // rot CCW 90 degree
+#define BG_COLOR WHITE
+
+inline void setup_spi() {
+  // SPI initialisation. This example will use SPI at 1MHz.
+  spi_init(SPI_PORT, 25 * 1000 * 1000);
+  gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+  gpio_set_function(PIN_CS, GPIO_FUNC_SIO);
+  gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+  gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+
+  gpio_set_dir(PIN_CS, GPIO_OUT);
+}
+
+// LCD_ST7735S LCD(SPI_PORT, PIN_CS, PIN_DC, PIN_RST, PIN_BL);
+LCD_ST7735S_buffered LCD(SPI_PORT, PIN_CS, PIN_DC, PIN_RST, PIN_BL);
+void LCD_Init() {
+  gpio_init(PIN_DC);
+  gpio_set_dir(PIN_DC, GPIO_OUT);
+  gpio_init(PIN_RST);
+  gpio_set_dir(PIN_RST, GPIO_OUT);
+  gpio_init(PIN_BL);
+  gpio_set_dir(PIN_BL, GPIO_OUT);
+
+  LCD.LCD_Init(SCAN_DIR);
+}
+
+int main() {
+  stdio_init_all();
+
+  setup_spi();
+
+  LCD_Init();
+
+  LCD_COLOR bg_color = BLACK;
+
+  LCD.LCD_Clear(bg_color);
+
+  sleep_ms(2000);
+
+  int h = 0;
+  while (true) {
+    display::draw_color_selector(&LCD, h, 40, 30, 40, 30, 10, 4, WHITE,
+                                 bg_color);
+    h += 2;
+    h %= 360;
+    sleep_ms(16);
+  }
+
+  return 0;
+}
